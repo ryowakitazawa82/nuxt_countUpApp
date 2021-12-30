21889c2cbf6cb2965a230dd3957989818d93df55
ryowa.kitazawa <ryowa.kitazawa@gmail.com>
2021-12-30 12:56:11 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <Counter></Counter>
+    <hr>
+    <Counter></Counter>
+    <hr>
+    <Counter></Counter>
+  </section>
+</template>
+
+
+<script>
+import Counter from '~/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
